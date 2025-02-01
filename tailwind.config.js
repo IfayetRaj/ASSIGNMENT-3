f6072258a4e2531fccacdb230b8460562200842b
IfayetRaj <a.s.m.rajuddin@gmail.com>
2025-02-01 20:47:32 +0600

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./**/*.html"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
